f46583cb329bf42a71dfe189e79a59f8d75cab30
Joangie Márquez <practicatto@gmail.com>
2022-11-14 21:03:41 -0500

    add requested changes

--- src/routes/Chocomision.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  export let idx: number;
-  export let content: string;
-
-  const is_first = idx === 0;
-  const current_num = (idx + 1).toString().padStart(2, "0");
-</script>
