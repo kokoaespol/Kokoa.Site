1537bb3f5c669e2874e2983df252b12dd62bbf72
Adrian Delgado <git@adrianjdelgado.com>
2023-01-02 19:44:03 -0500

    Update packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
 import cloudflare from "@sveltejs/adapter-cloudflare";
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: preprocess({
-    postcss: true,
-  }),
+  preprocess: vitePreprocess(),
 
   kit: {
     adapter: cloudflare(),
