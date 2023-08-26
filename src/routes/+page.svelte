183798a674158070cb3a4f9c4e15a72bc1611ab1
practicatto <practicatto@gmail.com>
2023-08-26 11:30:00 -0500

    add kokoa to h1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-<div class="container h-full mx-auto flex justify-center items-center">
+<div class="container mx-auto flex h-full items-center justify-center">
   <div class="space-y-5">
-    <h1 class="h1">Let's get cracking bones!</h1>
+    <h1 class="h1">Kokoa</h1>
     <p>Start by exploring:</p>
     <ul>
       <li>
