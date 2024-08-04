da81f1315587fd0687c013c257b496768a75cfa6
BryanEstrada003 <mbestrad@espol.edu.ec>
2024-08-04 17:31:25 -0500

    Create component Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,10 @@
 	import "../app.postcss";
 	import { Toaster } from "svelte-sonner";
 	import { page } from "$app/stores";
+	import Navbar from "./components/Navbar.svelte";
 
 	let { children } = $props();
 </script>
-
 
 
 <svelte:head>
@@ -13,12 +13,7 @@
 	<meta name="description" content={$page.data.description} />
 </svelte:head>
 
-<nav>
-	<a href="/">Inicio</a>
-	<a href="/eventos">Eventos</a>
-	<a href="/proyectos">Proyectos</a>
-	<a href="/contacto">Contacto</a>
-	<a href="/miembros">Miembros</a>
-</nav>
-<!--<Toaster richColors closeButton />-->
+<Navbar/>
+
+<Toaster richColors closeButton />
 {@render children()}
---
+++ src/routes/components/Navbar.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/">Inicio</a>
+	<a href="/events">Eventos</a>
+	<a href="/projects">Proyectos</a>
+	<a href="/contact">Contacto</a>
+	<a href="/member">Miembros</a>
+</nav>
