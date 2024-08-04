e991a20916a595ab1991ddf6e223e3be967e13dd
Adrian Delgado <git@adrianjdelgado.com>
2024-08-04 16:55:39 -0500

    Add TODOs

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -13,15 +13,17 @@
 	let { term, name, category, description /* , authors */ }: Props = $props();
 </script>
 
+<!-- TODO: Use semantic CSS https://www.magentaa11y.com/checklist-web/link/#complex-examples -->
 <div class="m-4 max-w-72 rounded-2xl bg-neutral-800 px-6 py-4">
 	<div class="mb-4">
 		<span class="font-fira font-medium text-lime-500">{term}</span>
 		<h2 class="font-fira text-xl font-medium text-white">{name}</h2>
 	</div>
 	<Tag {category} />
-	<p class="font-content my-4 text-[#dee5f2]">{description}</p>
+	<p class="my-4 font-content text-[#dee5f2]">{description}</p>
 	<div class="flex w-full flex-row justify-between">
 		<div class="flex flex-row items-center justify-center">
+			<!-- TODO: Implement author's avatars -->
 			<!-- {#each authors as author}
 				<img src="https://via.placeholder.com/150" alt={author} class="h-4 w-4 rounded-full" />
 			{/each} -->
