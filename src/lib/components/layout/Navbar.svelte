d302b57c32e6d8f77c3434b7d709080a03c8dcad
Adrian Delgado <git@adrianjdelgado.com>
2024-08-05 16:24:46 -0500

    Refactor Navbar again

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from "$app/stores";
-	import logo from "$lib/assets/images/logo.png";
+	import gecko_code from "$lib/assets/logos/gecko-code.svg";
 
 	const current_path = $derived($page.url.pathname);
 
@@ -14,20 +14,20 @@
 </script>
 
 <nav class="flex items-center justify-between p-4">
-	<img src={logo} alt="Logo de Kokoa" width="40" height="40" />
+	<img src={gecko_code} alt="Logo de Kokoa" width="52.9" height="24.4" />
 	<div class="flex gap-x-8 text-lg">
 		{#each LINKS as { href, text } (href)}
-			<a {href} class="relative font-bold hover:underline">
+			<a {href} class="relative font-fira hover:underline">
 				<span
 					aria-hidden="true"
-					class="absolute -left-2 text-green-400 {current_path !== href ? 'hidden' : ''}"
+					class="absolute -left-3 text-lime-500 {current_path !== href ? 'hidden' : ''}"
 				>
 					{"{"}
 				</span>
 				{text}
 				<span
 					aria-hidden="true"
-					class="absolute -right-2 text-green-400 {current_path !== href ? 'hidden' : ''}"
+					class="absolute -right-3 text-lime-500 {current_path !== href ? 'hidden' : ''}"
 				>
 					{"}"}
 				</span>
