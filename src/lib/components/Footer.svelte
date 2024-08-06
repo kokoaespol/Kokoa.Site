ba0d418f4ec2e982132d8ef927aa2be2ca32329e
Adrian Delgado <git@adrianjdelgado.com>
2024-08-06 00:45:38 -0500

    Refactor a bit

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,39 +1,47 @@
-<script src="https://cdn.jsdelivr.net/npm/simple-icons@13.4.0/index.min.js">
-    import { siFacebook, siX, siInstagram, siGithub } from 'simple-icons';
+<script lang="ts">
+	import facebook_logo from "$lib/assets/icons/facebook.svg";
+	import x_logo from "$lib/assets/icons/x.svg";
+	import instagram_logo from "$lib/assets/icons/instagram.svg";
+	import github_logo from "$lib/assets/icons/github.svg";
 </script>
 
 <footer>
-    <div class="flex flex-col gap-6 justify-between p-6">
+	<div class="flex flex-col justify-between gap-6 p-6">
+		<div class="flex justify-between sm:flex-col md:flex-row md:gap-96">
+			<!-- TODO: use cute gecko -->
+			<img class="invisible md:visible" src="" alt="Mascota" />
+			<div class="flex flex-col gap-2 lg:justify-between">
+				<h2>SUBSCRIBE TO OUR NEWSLETTER</h2>
+				<p>The latest news, articles, and resources, sent to your inbox weekly.</p>
+				<div class="flex flex-col justify-between gap-2 md:flex-row">
+					<input
+						class="form-input rounded px-1 text-gray-900 md:h-full md:w-3/4 md:px-4"
+						type="email"
+						placeholder="Enter your email"
+					/>
+					<button class="m-2 w-1/3 truncate rounded-lg bg-lime-600 p-3 md:w-1/4">
+						Subscribe
+					</button>
+				</div>
+			</div>
+		</div>
 
-        <div class="flex sm:flex-col md:flex-row md:gap-96 justify-between">
-            <img class="invisible md:visible" src="" alt="Mascota"/>
-            <div class="flex flex-col gap-2 lg:justify-between" >
-                <h2>SUBSCRIBE TO OUR NEWSLETTER</h2>
-                <p>The latest news, articles, and resources, sent to your inbox weekly.</p>
-                <div class="flex flex-col md:flex-row gap-2 justify-between ">
-                    <input class="w-1/1 h-10 md:h-full md:w-3/4 text-black pr-1 pl-1 md:pr-4 md:pl-4 rounded" type="email" placeholder="Enter your email"/>
-                    <button class="text-white bg-green-600 rounded-lg p-3 m-2 w-1/3 md:w-1/4 truncate">Subscribe</button>
-                </div>
-            </div>
-        </div>
-
-        <div class="flex lg:gap-96 justify-between border-t border-t-gray-400 p-4" >
-            <h6 class="text-gray-300 text-[0.5rem] md:text-md">© 2022 Kokoa, Inc. All rights reserved.</h6>
-            <div class="flex gap-6 text-gray-300">
-                <span class="icon-[mdi--facebook]"></span>
-                <a href="https://www.facebook.com/profile.php?id=100081089247661&locale=es_LA">
-                    <img height="32" width="32" src="https://cdn.simpleicons.org/facebook/white" alt="Facebook" class= "w-4 h-4"/>
-                </a>
-                <a href="https://x.com/kokoaespol" >
-                    <img height="32" width="32" src="https://cdn.simpleicons.org/x/white" alt="X" class= "w-4 h-4"/>
-                </a>
-                <a href="https://www.instagram.com/kokoa_espol/?hl=es" >
-                    <img height="32" width="32" src="https://cdn.simpleicons.org/instagram/white" alt="Instagram" class= "w-4 h-4"/>
-                </a>
-                <a href="https://github.com/kokoaespol" >
-                    <img height="32" width="32" src="https://cdn.simpleicons.org/github/white" alt="Github" class= "w-4 h-4"/>
-                </a>
-            </div>
-        </div>
-    </div>
+		<div class="flex justify-between border-t border-t-gray-400 p-4 lg:gap-96">
+			<h6 class="text-xs text-gray-300">© 2024 Kokoa</h6>
+			<div class="flex gap-6 text-gray-300">
+				<a href="https://www.facebook.com/kokoaecuador">
+					<img height="16" width="16" src={facebook_logo} alt="Facebook" />
+				</a>
+				<a href="https://x.com/kokoa_espol">
+					<img height="16" width="16" src={x_logo} alt="X" />
+				</a>
+				<a href="https://www.instagram.com/kokoa_espol">
+					<img height="16" width="16" src={instagram_logo} alt="Instagram" />
+				</a>
+				<a href="https://github.com/kokoaespol">
+					<img height="16" width="16" src={github_logo} alt="Github" />
+				</a>
+			</div>
+		</div>
+	</div>
 </footer>
