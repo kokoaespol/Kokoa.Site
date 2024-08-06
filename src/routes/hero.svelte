1237b091239566b28f605d7dd58ec914515332aa
Alina Carpio <agcarpio@espol.edu.ec>
2024-08-06 13:07:05 -0500

    Corrected the code with prettier

--- src/routes/hero.svelte
+++ src/routes/hero.svelte
@@ -1,32 +1,33 @@
-<div class=" flex flex-col lg:flex-row gap-y-12 lg:flex justify-around mt-24 items-center">
+<div class=" mt-24 flex flex-col items-center justify-around gap-y-12 lg:flex lg:flex-row">
 	<div class="relative">
 		<img
 			src="https://images.unsplash.com/photo-1601933470096-0e34634ffcde?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 			alt="Imagen de amigos programando"
-			class="w-[16rem] h-[12.5rem] lg:w-[26rem] lg:h-[18rem] relative z-10"
+			class="relative z-10 h-[12.5rem] w-[16rem] lg:h-[18rem] lg:w-[26rem]"
 		/>
 		<div
-			class="absolute top-[-0.5rem] left-[-0.5rem] w-5 h-5 lg:top-[-1.3rem] lg:left-[-1.3rem] lg:w-[6rem] lg:h-[6rem] z-0 bg-primary"></div>
+			class="absolute left-[-0.5rem] top-[-0.5rem] z-0 h-5 w-5 bg-primary lg:left-[-1.3rem] lg:top-[-1.3rem] lg:h-[6rem] lg:w-[6rem]"
+		></div>
 		<div
-			class="absolute bottom-[-0.5rem] right-[-0.5rem] w-5 h-5 lg:bottom-[-1.2rem] lg:right-[-1.2rem] lg:w-[6rem] lg:h-[6rem] z-0 border border-primary"></div>
+			class="absolute bottom-[-0.5rem] right-[-0.5rem] z-0 h-5 w-5 border border-primary lg:bottom-[-1.2rem] lg:right-[-1.2rem] lg:h-[6rem] lg:w-[6rem]"
+		></div>
 	</div>
-
 	<div class="flex flex-col items-center gap-y-5">
 		<div>
 			<span class="text-primary lg:text-xl">Kokoa</span>
-			<span class="text-base font-fira lg:text-xl">- Vive el software libre</span>
+			<span class="font-fira text-base lg:text-xl">- Vive el software libre</span>
 		</div>
 		<div class="max-w-screen-md lg:max-w-screen-sm">
-			<p class="text-base text-sm font-fira lg:text-md text-center">Club estudiantil politécnico promotor del
-				uso, modificación, distribución y aprendizaje de software libre </p>
+			<p class="lg:text-md text-center font-fira text-base text-sm">
+				Club estudiantil politécnico promotor del uso, modificación, distribución y aprendizaje de
+				software libre
+			</p>
 		</div>
 		<button
-			class="button-shd px-14 py-1 font-medium bg-primary w-15 border-primary border-2xl rounded-2xl text-black shadow-2xl">
+			class="button-shd w-15 border-2xl rounded-2xl border-primary bg-primary px-14 py-1 font-medium text-black shadow-2xl"
+		>
 			Unirse
 		</button>
 		<div class="bg-green-400"></div>
 	</div>
 </div>
-
-
-
