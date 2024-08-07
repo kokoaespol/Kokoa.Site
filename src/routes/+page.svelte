61342d8b1a807167817044caca639fb32e555c0a
Alina Carpio <72272077+alicarpio@users.noreply.github.com>
2024-08-07 12:12:55 -0500

    Add hero part (#48)
    * Add hero part
    
    * Corrected the code with prettier
    
    * Avoid using custom classes
    
    * Follow style guide
    
    * Use semantic HTML
    
    ---------
    
    Co-authored-by: Adrian Delgado <git@adrianjdelgado.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class="font-mono text-5xl">Bienvenido a KOKOA</h1>
-<p class="font-mono">Promovemos la programación orientada al comunismo</p>
+<script lang="ts">
+	import Hero from "./Hero.svelte";
+</script>
+
+<Hero />
