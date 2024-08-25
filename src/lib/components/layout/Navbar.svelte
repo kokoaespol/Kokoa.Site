3e638b9689e8e919dde586b7d6139c31a6a298fc
Alexander Goussas <goussasalexander@gmail.com>
2024-08-24 23:44:07 -0500

    feature: Unroll loops to ease the toll on the Rust compiler

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -11,27 +11,27 @@
 		{ text: "Contacto", href: "/contact" },
 		{ text: "Miembros", href: "/members" },
 	];
+
+	const { 0: link1, 1: link2, 2: link3, 3: link4, 4: link5 } = LINKS;
 </script>
 
 <nav class="flex items-center justify-between p-4">
 	<img src={gecko_code} alt="Logo de Kokoa" width="52.9" height="24.4" />
 	<div class="flex gap-x-8 text-lg">
-		{#each LINKS as { href, text } (href)}
-			<a {href} class="relative font-fira hover:underline">
-				<span
-					aria-hidden="true"
-					class="absolute -left-3 text-lime-500 {current_path !== href ? 'hidden' : ''}"
-				>
-					{"{"}
-				</span>
-				{text}
-				<span
-					aria-hidden="true"
-					class="absolute -right-3 text-lime-500 {current_path !== href ? 'hidden' : ''}"
-				>
-					{"}"}
-				</span>
-			</a>
-		{/each}
+		<a {link1.href} class="relative font-fira hover:underline">
+			<span
+				aria-hidden="true"
+				class="absolute -left-3 text-lime-500 {current_path !== href ? 'hidden' : ''}"
+			>
+				{"{"}
+			</span>
+			{link1.text}
+			<span
+				aria-hidden="true"
+				class="absolute -right-3 text-lime-500 {current_path !== href ? 'hidden' : ''}"
+			>
+				{"}"}
+			</span>
+		</a>
 	</div>
 </nav>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -2,16 +2,15 @@
 	import CommonCard from "$lib/components/CommonCard.svelte";
 
 	let { data } = $props();
+	let { 0: project } = data.projects;
 </script>
 
 <p class="font-medium">{data.description}</p>
 <div class="mt-8 flex flex-col items-center sm:flex sm:flex-row sm:flex-wrap">
-	{#each data.projects as project}
-		<CommonCard
-			term={project.term}
-			name={project.name}
-			category={project.category}
-			description={project.description}
-		/>
-	{/each}
+	<CommonCard
+		term={project.term}
+		name={project.name}
+		category={project.category}
+		description={project.description}
+	/>
 </div>
