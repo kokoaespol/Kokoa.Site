720ccca103cdf406902d711a800b7b9639863eb4
MelissaAyllon <mayllon@espol.edu.ec>
2024-09-03 21:23:36 -0500

    skeleton member component

---
+++ src/lib/components/Member.svelte
@@ -0,0 +1,20 @@
+<div>
+    <img src="" alt="">
+
+    <div>
+        <h2>Member Name</h2>
+        <p>Member Role</p>
+    </div>
+
+    <div>
+        <a href="">
+            <img>
+        </a>
+        <a href="">
+            <img>
+        </a>
+        <a href="">
+            <img>
+        </a>
+    </div>
+</div>
