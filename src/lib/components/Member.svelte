b05dbf7fbece82e2b190928edd921f12dd1e8525
MelissaAyllon <mayllon@espol.edu.ec>
2024-09-05 21:37:52 -0500

    member component

--- src/lib/components/Member.svelte
+++ src/lib/components/Member.svelte
@@ -1,20 +1,42 @@
+<script lang="ts">
+    import github_logo from "$lib/assets/icons/github.svg";
+    import linkedin_logo from "$lib/assets/icons/linkedin.svg";
+    import link_logo from "$lib/assets/icons/bx-link.svg";
+
+    type Props = { 
+        name: string;
+	    role: string;
+		photo: string;
+		socialMedia: {nombre: string, link: string}[];
+    }
+
+    export let props: Props;
+</script>
+
 <div>
-    <img src="" alt="">
+    <img src={props.photo} alt="Foto">
 
     <div>
-        <h2>Member Name</h2>
-        <p>Member Role</p>
+        <h2>{props.name}</h2>
+        <p>{props.role}</p>
     </div>
 
     <div>
-        <a href="">
-            <img>
-        </a>
-        <a href="">
-            <img>
-        </a>
-        <a href="">
-            <img>
-        </a>
+
+        {#each props.socialMedia as media}
+            
+            <a href={media.link}>
+
+                {#if media.nombre === "linkedin"}
+                    <img src={linkedin_logo} alt="LinkedIn">
+                {:else if media.nombre === "github"}
+                    <img src={github_logo} alt="Github">
+                {:else if media.nombre === "link"}
+                    <img src={link_logo} alt="Link">
+                {/if}
+
+            </a>
+
+        {/each}
     </div>
 </div>
