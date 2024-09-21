0eccab4557564606529630b72089fac82ada7186
alicarpio <agcarpio@espol.edu.ec>
2024-09-21 14:07:23 -0500

    Add responsive chocomision component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Hero from "./Hero.svelte";
+	import Chocomision from "./Chocomision.svelte";
 </script>
 
 <Hero />
+<Chocomision />
---
+++ src/routes/Chocomision.svelte
@@ -0,0 +1,13 @@
+<section
+	class="flex flex-col sm:flex-row md:flex-row lg:flex-row items-center justify-around mt-8 lg:mt-5 w-full"
+	aria-label="Sección de misión y logo">
+	<p id="mision"
+		 class="text-center text-sm max-w-[19rem] md:text-base lg:text-[1rem] sm:max-w-[20rem] md:max-w-[23rem] lg:max-w-[25rem]">
+		Compartir, difundir e implementar, conocimiento y herramientas, para el desarrollo académico y
+		tecnológico de la sociedad Ecuatoriana
+	</p>
+	<img id="logo_kokoa"
+			 src="/src/lib/assets/logos/logo-semib.png" alt="Logo de KOKOA"
+			 class="w-36 mt-4 sm:w-44 sm:mt-0 md:w-48 lg:w-64 sm:mr-10 md:mr-15 lg:mr-[4.9rem]">
+</section>
+
