0884977e1a86b59d71a0637a3c28f9104e56a6b2
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-09-21 16:53:26 -0500

    pass category as prop

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
-	import Event from "$lib/components/HomeEvent.svelte";
+	import Event from "$lib/components/Event.svelte";
 
 	let { data } = $props();
 </script>
 
-<div class="flex flex-col items-center px-8">
+<div class="flex flex-wrap items-center px-8">
 	{#each data.events as event}
 		<Event
 			name={event.name}
 			description={event.description}
 			date={event.date}
-			time={event.time}
 			place={event.place}
+			category={event.category}
 		/>
 	{/each}
 </div>
