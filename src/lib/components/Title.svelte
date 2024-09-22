a4ef8373e959e62dd4245c354c2ad0f8af0c5087
alicarpio <agcarpio@espol.edu.ec>
2024-09-21 23:15:31 -0500

    Add title

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -6,6 +6,6 @@
 	let { title }: Props = $props();
 </script>
 
-<h2 class="font-fira text-xl font-semibold">
+<h2 class="font-fira text-xl font-semibold ml-24">
 	<span class="text-lime-500">></span>{title}
 </h2>
--- src/routes/Chocomision.svelte
+++ src/routes/Chocomision.svelte
@@ -1,18 +1,25 @@
+<script>
+	import Title from "$lib/components/Title.svelte";
+</script>
+
 <section
-	class="mt-8 flex w-full flex-col items-center justify-around sm:flex-row md:flex-row lg:mt-5 lg:flex-row"
-	aria-label="Sección de misión y logo"
+	class="mt-20"
 >
-	<p
-		id="mision"
-		class="max-w-[19rem] text-center text-sm sm:max-w-[20rem] md:max-w-[23rem] md:text-base lg:max-w-[25rem] lg:text-[1rem]"
-	>
-		Compartir, difundir e implementar, conocimiento y herramientas, para el desarrollo académico y
-		tecnológico de la sociedad Ecuatoriana
-	</p>
-	<img
-		id="logo_kokoa"
-		src="/src/lib/assets/logos/logo-semib.png"
-		alt="Logo de KOKOA"
-		class="md:mr-15 mt-4 w-36 sm:mr-10 sm:mt-0 sm:w-44 md:w-48 lg:mr-[4.9rem] lg:w-64"
-	/>
+	<Title title="Chocoeventos" />
+	<div class="flex w-full flex-col items-center justify-between sm:flex-row md:flex-row lg:mt-5 lg:flex-row">
+		<p
+			id="mision"
+			class="max-w-[19rem] text-sm sm:max-w-[20rem] md:max-w-[23rem] lg:ml-24 md:text-base lg:max-w-[25rem] lg:text-[1rem]"
+		>
+			Compartir, difundir e implementar, conocimiento y herramientas, para el desarrollo académico y
+			tecnológico de la sociedad Ecuatoriana
+		</p>
+		<img
+			id="logo_kokoa"
+			src="/src/lib/assets/logos/logo-semib.png"
+			alt="Logo de KOKOA"
+			class="md:mr-15 mt-4 w-36 sm:mr-10 sm:mt-0 sm:w-44 md:w-48 lg:mr-[4.9rem] lg:w-64"
+		/>
+	</div>
+
 </section>
