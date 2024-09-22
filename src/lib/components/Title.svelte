fc877fe8a03bf4c2fc9b3208a2a4573237e88349
alicarpio <agcarpio@espol.edu.ec>
2024-09-22 00:19:50 -0500

    format code

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -6,6 +6,6 @@
 	let { title }: Props = $props();
 </script>
 
-<h2 class="font-fira text-2xl font-semibold text-center sm:text-left md:text-left lg:text-left">
+<h2 class="text-center font-fira text-2xl font-semibold sm:text-left md:text-left lg:text-left">
 	<span class="text-lime-500">></span>{title}
 </h2>
--- src/routes/Chocomision.svelte
+++ src/routes/Chocomision.svelte
@@ -3,13 +3,13 @@
 </script>
 
 <section
-	class="mt-10 lg:mt-16 flex flex-col items-center sm:flex-row md:flex-row lg:flex-row lg:items-center "
+	class="mt-10 flex flex-col items-center sm:flex-row md:flex-row lg:mt-16 lg:flex-row lg:items-center"
 >
-	<div class="flex w-full flex-col justify-between sm:flex-col md:flex-col lg:flex-col ">
+	<div class="flex w-full flex-col justify-between sm:flex-col md:flex-col lg:flex-col">
 		<Title title="Chocomisión" />
 		<p
 			id="mision"
-			class="max-w-[20rem] text-center md:text-left mt-4 lg:mt-5 sm:text-left lg:text-left text-sm sm:max-w-[20rem] lg:ml-3 md:max-w-[23rem] md:text-base lg:max-w-[25rem] lg:text-[1rem]"
+			class="mt-4 max-w-[20rem] text-center text-sm sm:max-w-[20rem] sm:text-left md:max-w-[23rem] md:text-left md:text-base lg:ml-3 lg:mt-5 lg:max-w-[25rem] lg:text-left lg:text-[1rem]"
 		>
 			Compartir, difundir e implementar, conocimiento y herramientas, para el desarrollo académico y
 			tecnológico de la sociedad Ecuatoriana.
@@ -19,6 +19,6 @@
 		id="logo_kokoa"
 		src="/src/lib/assets/logos/logo-semib.png"
 		alt="Logo de KOKOA"
-		class="md:mr-15 mt-4 w-44 sm:mt-0 sm:w-44 md:w-48  md:mr-[7rem] lg:w-72"
+		class="md:mr-15 mt-4 w-44 sm:mt-0 sm:w-44 md:mr-[7rem] md:w-48 lg:w-72"
 	/>
 </section>
