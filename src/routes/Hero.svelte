cec5b90329dba86f5276fadeb7c118a2423c5c5c
Alina Carpio <72272077+alicarpio@users.noreply.github.com>
2024-09-21 23:14:43 -0500

    Add hero responsive (#55)

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,33 +1,30 @@
-<section class="mt-24 flex flex-col items-center justify-around gap-y-12 lg:flex lg:flex-row">
+<section class="mt-24 flex flex-col items-center justify-around gap-y-12 sm:flex-row lg:flex-row">
 	<div class="relative">
 		<img
 			src="https://images.unsplash.com/photo-1601933470096-0e34634ffcde?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 			alt="Imagen de amigos programando"
-			class="relative z-10 h-[12.5rem] w-[16rem] lg:h-[18rem] lg:w-[26rem]"
+			class="relative z-10 h-[12.5rem] w-[16rem] object-cover lg:h-[18rem] lg:w-[26rem]"
 		/>
 		<div
-			class="absolute left-[-0.5rem] top-[-0.5rem] z-0 h-5 w-5 bg-lime-500 lg:left-[-1.3rem] lg:top-[-1.3rem] lg:h-[6rem] lg:w-[6rem]"
+			class="absolute left-[-0.5rem] top-[-0.5rem] z-0 h-5 w-5 bg-lime-500 lg:left-[-1.3rem] lg:top-[-1.3rem] lg:h-24 lg:w-24"
 		></div>
 		<div
-			class="absolute bottom-[-0.5rem] right-[-0.5rem] z-0 h-5 w-5 border border-lime-500 lg:bottom-[-1.2rem] lg:right-[-1.2rem] lg:h-[6rem] lg:w-[6rem]"
+			class="absolute bottom-[-0.5rem] right-[-0.5rem] z-0 h-5 w-5 border border-lime-500 lg:bottom-[-1.2rem] lg:right-[-1.2rem] lg:h-24 lg:w-24"
 		></div>
 	</div>
-	<div class="flex flex-col items-center gap-y-5">
-		<h1>
-			<span class="text-lime-500 lg:text-xl">Kokoa</span>
-			<span class="font-fira text-blue-100 lg:text-xl">- Vive el software libre</span>
+	<div class="flex max-w-[20rem] flex-col items-center gap-y-5 lg:max-w-[40rem]">
+		<h1 class="font-fira text-[1.11rem] font-bold md:text-[1.rem] lg:text-[1.55rem]">
+			<span class="text-lime-500">Kokoa</span>
+			<span class="text-blue-100">- Vive el software libre</span>
 		</h1>
-		<div class="max-w-screen-md lg:max-w-screen-sm">
-			<p class="lg:text-md text-center font-fira text-sm text-blue-100">
-				Club estudiantil politécnico promotor del uso, modificación, distribución y aprendizaje de
-				software libre
-			</p>
-		</div>
+		<p class="text-center text-[0.85rem] text-blue-100 lg:text-base">
+			Club estudiantil politécnico promotor del uso, modificación, distribución y aprendizaje de
+			software libre
+		</p>
 		<button
-			class="w-15 border-2xl rounded-2xl border-lime-500 bg-lime-500 px-14 py-1 font-medium text-black shadow-2xl"
+			class="w-[10rem] rounded-3xl bg-lime-500 px-14 py-2 font-medium text-black shadow-2xl transition-colors hover:bg-lime-600 sm:w-auto"
 		>
 			Unirse
 		</button>
-		<div class="bg-green-400"></div>
 	</div>
 </section>
