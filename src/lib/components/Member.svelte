88f234e4623d38987f4e27dc277743f443f39196
Alexander Goussas <84427521+aloussase@users.noreply.github.com>
2024-09-22 21:52:05 -0500

    Format chocomiembros (#64)
    * ci: Configure for deployment on premise
    
    * feature: Add projects page
    
    * fix: Fix linter warnings
    
    * fix: Revert to using adapter-cloudflare for main integration
    
    * feature: Update Heading component to use a slot
    
    * ci: Use adapter-node
    
    * chore: Apply formatting
    
    ---------
    
    Co-authored-by: Alexander Goussas <alexander.goussas@tia.com.ec>

--- src/lib/components/Member.svelte
+++ src/lib/components/Member.svelte
@@ -1,42 +1,49 @@
 <script lang="ts">
-    import github_logo from "$lib/assets/icons/github.svg";
-    import linkedin_logo from "$lib/assets/icons/linkedin.svg";
-    import link_logo from "$lib/assets/icons/bx-link.svg";
+	import github_logo from "$lib/assets/icons/github.svg";
+	import linkedin_logo from "$lib/assets/icons/linkedin.svg";
+	import link_logo from "$lib/assets/icons/bx-link.svg";
 
-    type Props = { 
-        name: string;
-	    role: string;
+	type Props = {
+		name: string;
+		role: string;
 		photo: string;
-		socialMedia: {nombre: string, link: string}[];
-    }
+		socialMedia: { nombre: string; link: string }[];
+	};
 
-    let { name, role, photo, socialMedia}:Props = $props();
+	let { name, role, photo, socialMedia }: Props = $props();
 </script>
 
-<div class="flex flex-col gap-2 justify-center items-center pb-14">
-    <img class="rounded-full size-20 md:size-24 inset-0" src={photo} alt="Foto" >
+<div class="flex flex-col items-center justify-center gap-2 pb-14">
+	<img class="inset-0 size-20 rounded-full md:size-24" src={photo} alt="Foto" />
 
-    <div class="flex flex-col justify-center items-center">
-        <h2 class="text-lime-400 text-base" >{name}</h2>
-        <p class="text-sm">{role}</p>
-    </div>
+	<div class="flex flex-col items-center justify-center">
+		<h2 class="text-base text-lime-400">{name}</h2>
+		<p class="text-sm">{role}</p>
+	</div>
 
-    <div class="flex flex-row gap-2 justify-items-center object-center	">
-
-        {#each socialMedia as media}
-            
-            <a class="object-center" href={media.link}>
-
-                {#if media.nombre === "linkedin"}
-                    <img class="basis-1/3 object-center	" height="16" width="16" src={linkedin_logo} alt="LinkedIn">
-                {:else if media.nombre === "github"}
-                    <img class="basis-1/3 object-center	" height="16" width="16" src={github_logo} alt="Github">
-                {:else if media.nombre === "link"}
-                    <img class="basis-1/3 object-center	" height="16" width="16" src={link_logo} alt="Link">
-                {/if}
-
-            </a>
-
-        {/each}
-    </div>
+	<div class="flex flex-row justify-items-center gap-2 object-center">
+		{#each socialMedia as media}
+			<a class="object-center" href={media.link}>
+				{#if media.nombre === "linkedin"}
+					<img
+						class="basis-1/3 object-center"
+						height="16"
+						width="16"
+						src={linkedin_logo}
+						alt="LinkedIn"
+					/>
+				{:else if media.nombre === "github"}
+					<img
+						class="basis-1/3 object-center"
+						height="16"
+						width="16"
+						src={github_logo}
+						alt="Github"
+					/>
+				{:else if media.nombre === "link"}
+					<img class="basis-1/3 object-center" height="16" width="16" src={link_logo} alt="Link" />
+				{/if}
+			</a>
+		{/each}
+	</div>
 </div>
--- src/lib/components/Members_home.svelte
+++ src/lib/components/Members_home.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import Member from "$lib/components/Member.svelte"
-	import members from "$lib/data/members.json"
+	import Member from "$lib/components/Member.svelte";
+	import members from "$lib/data/members.json";
 </script>
 
-<div class="grid grid-cols-1 md:grid-cols-3 justify-center px-20 md:px-48 py-48 justify-items-center">
+<div
+	class="grid grid-cols-1 justify-center justify-items-center px-20 py-48 md:grid-cols-3 md:px-48"
+>
 	{#each members as member}
-		<Member 
-			name={member.name} 
+		<Member
+			name={member.name}
 			role={member.role}
 			photo={member.photo}
 			socialMedia={member.socialMedia}
