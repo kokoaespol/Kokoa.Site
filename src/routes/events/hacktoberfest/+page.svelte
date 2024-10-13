7b41e3183c5d3d650c7aa3f33f613abda8331703
Brayen Gavilanes <bjgavilanes@gmail.com>
2024-10-13 11:39:53 +0000

    pnpm: pnpm run lint
    GitHub Actions was complaining about a botched `pnpm run lint` job.
    
    This should do.

--- src/routes/events/hacktoberfest/+page.svelte
+++ src/routes/events/hacktoberfest/+page.svelte
@@ -13,20 +13,19 @@
 	<Tag category="11-11-2024 | 15-11-2024" />
 
 	<p id="description">
-		Hacktoberfest is a month-long celebration of open-source
-		software, held annually in October. The event encourages
-		participants to contribute to open-source projects,
-		rewarding them with prizes and recognition for their
-		contributions.
+		Hacktoberfest is a month-long celebration of open-source software, held annually in October. The
+		event encourages participants to contribute to open-source projects, rewarding them with prizes
+		and recognition for their contributions.
 	</p>
 
 	<h4 class="text-xl text-lime-600">How to participate?</h4>
 
 	<p id="how-to-participate">
-		To participate, you simply need to register on the <a
-		href="http://hacktoberfestgye.org/">Hacktoberfest</a> website and
-		start making contributions to open-source projects on
-		platforms like <a href="https://github.com/Hacktoberfest-GYE">GitHub</a>.
+		To participate, you simply need to register on the <a href="http://hacktoberfestgye.org/"
+			>Hacktoberfest</a
+		>
+		website and start making contributions to open-source projects on platforms like
+		<a href="https://github.com/Hacktoberfest-GYE">GitHub</a>.
 	</p>
 
 	<p>You can contribute in many ways, such as:</p>
@@ -46,17 +45,17 @@
 		<li>Resolving issues reported by other users</li>
 	</ul>
 
-	<div id="registration" class="flex flex-col border rounded-md p-2 shadow-lg">
-		<h3 class="text-center text-2xl mb-4">Register</h3>
-		<div class="flex w-full gap-4 justify-center">
+	<div id="registration" class="flex flex-col rounded-md border p-2 shadow-lg">
+		<h3 class="mb-4 text-center text-2xl">Register</h3>
+		<div class="flex w-full justify-center gap-4">
 			<div id="registration-speakers" class="flex-1">
 				<a target="_blank" href="https://forms.gle/2mH4fi7n9ukJPXPr9">
-					<button class="rounded-md p-2 w-full bg-lime-600 hover:bg-lime-500">Speakers</button>
+					<button class="w-full rounded-md bg-lime-600 p-2 hover:bg-lime-500">Speakers</button>
 				</a>
 			</div>
 			<div id="registration-participants" class="flex-1">
 				<a target="_blank" href="https://forms.gle/Qyeoc46c8NSkqeNNA">
-					<button class="rounded-md p-2 w-full bg-lime-600 hover:bg-lime-500">Participants</button>
+					<button class="w-full rounded-md bg-lime-600 p-2 hover:bg-lime-500">Participants</button>
 				</a>
 			</div>
 		</div>
